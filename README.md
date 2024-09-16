0e22e516b44db74f194b580b0e924a8701e4188c
João Pedro <joao_garciajp@usp.br>
2024-09-16 11:56:42 -0300

    DH pronta

---
+++ README.md
@@ -0,0 +1,9 @@
+# Introdução
+Explicar sobre que é da aula de dinâmica e etc
+
+# Get started
+Explicar como começar
+
+# Authors
+## João Assuéro Rego de Alencar
+## João Pedro Baltieca Garcia
--- peter_corke/test.py
+++ peter_corke/test.py
@@ -4,16 +4,19 @@ import numpy as np
 
 print('Robot Toolbox version: '+ rtb.__version__)
 
+    
 def graspe_func():
+    
+    d1 = 5.0
+    d2 = 0.5
     
     
     graspeDH = rtb.DHRobot([
-        rtb.RevoluteDH(d=10.0, a=0.0, alpha=-np.pi/2.0), 
-        rtb.RevoluteDH(d=0.0, a=0.0, alpha=np.pi/2.0, offset=-np.pi/2.0), 
-        rtb.PrismaticDH(theta=-np.pi/2.0,a=0.0, alpha=0.0), 
-        rtb.RevoluteDH(d=0.0, a=0.0, alpha=-np.pi/2),
-        rtb.RevoluteDH(d=0.0, a=0.0, alpha=np.pi/2),
-        rtb.RevoluteDH(d=5.0, a=0.0, alpha=0.0)
+        rtb.RevoluteDH(d=d1, a=0.0, alpha=-np.pi/2.0), 
+        rtb.RevoluteDH(d=d2, a=0.0, alpha=np.pi/2.0, offset=0), 
+        rtb.PrismaticDH(theta=0.0,a=0.0, alpha=np.pi/2.0), 
+        rtb.RevoluteDH(d=d2, a=0.0, alpha=-np.pi/2),
+        rtb.RevoluteDH(d=0.5, a=0.0, alpha=0),
         ],
         name='Manipulador Graspe')
     
@@ -22,10 +25,9 @@ def graspe_func():
     q = [
         np.pi/4, 
         np.pi/4, 
-        10.0, 
-        np.pi/4,
-        np.pi/4,
-        np.pi/4
+        5.0, 
+        -np.pi/4,
+        0
         ] # Ângulos em radianos para juntas revolutas e deslocamento prismático
 
     # Cinemática direta para encontrar a pose final
