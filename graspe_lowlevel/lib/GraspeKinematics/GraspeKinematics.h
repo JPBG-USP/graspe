0f0a28148843dfc18cf46ed79f420b76e93b8250
João Pedro <joao_garciajp@usp.br>
2025-03-29 12:06:06 -0300

    Adding Platform IO project

---
+++ graspe_lowlevel/lib/GraspeKinematics/GraspeKinematics.h
@@ -0,0 +1,13 @@
+#ifndef _GRASPE_KINEMATICS_
+#define _GRASPE_KINEMATCIS_
+
+class GraspeKinematics
+{
+private:
+    /* data */
+public:
+    GraspeKinematics();
+    ~GraspeKinematics();
+};
+
+#endif 
